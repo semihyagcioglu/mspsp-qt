f047bac934d9965506f291a82244833de4bde500
Semih Yağcıoğlu <semihyagcioglu@yahoo.com>
2013-04-17 13:16:16 +0300

    Updated main.cpp
    Updated main.cpp to reflect the purpose of the inital commits.

--- app/src/mspsp-qt-app/main.cpp
+++ app/src/mspsp-qt-app/main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
+    // At this point the application is doing nothing, this is just a starting point for the upcoming commits.
     QApplication a(argc, argv);
     Widget w;
     w.show();
