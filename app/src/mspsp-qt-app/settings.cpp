22464a550a0eea97e9f854eba88764037459fc44
Semih Yağcıoğlu <semihyagcioglu@yahoo.com>
2013-05-17 20:27:18 +0300

    Added application and algorithm settings.

--- app/src/mspsp-qt-app/settings.cpp
+++ app/src/mspsp-qt-app/settings.cpp
@@ -9,3 +9,112 @@ Settings::Settings(int maximumScheduleLimit, int populationSize)
     this->SetMaximumScheduleLimit(maximumScheduleLimit);
     this->SetPopulationSize(populationSize);
 }
+
+std::string Settings::GetFilePath() const
+{
+    return filePath;
+}
+
+void Settings::SetFilePath(const std::string &value)
+{
+    filePath = value;
+}
+
+Settings::REPLACEMENT_METHOD Settings::GetReplacementMethod() const
+{
+    return replacementMethod;
+}
+
+void Settings::SetReplacementMethod(const REPLACEMENT_METHOD &value)
+{
+    replacementMethod = value;
+}
+
+Settings::MUTATION_METHOD Settings::GetMutationMethod() const
+{
+    return mutationMethod;
+}
+
+void Settings::SetMutationMethod(const MUTATION_METHOD &value)
+{
+    mutationMethod = value;
+}
+
+Settings::CROSS_OVER_METHOD Settings::GetCrossOverMethod() const
+{
+    return crossOverMethod;
+}
+
+void Settings::SetCrossOverMethod(const CROSS_OVER_METHOD &value)
+{
+    crossOverMethod = value;
+}
+
+Settings::SELECTION_METHOD Settings::GetSelectionMethod() const
+{
+    return selectionMethod;
+}
+
+void Settings::SetSelectionMethod(const SELECTION_METHOD &value)
+{
+    selectionMethod = value;
+}
+
+int Settings::GetEliteSize() const
+{
+    return eliteSize;
+}
+
+void Settings::SetEliteSize(int value)
+{
+    eliteSize = value;
+}
+
+bool Settings::GetIsElitesAllowed() const
+{
+    return isElitesAllowed;
+}
+
+void Settings::SetIsElitesAllowed(bool value)
+{
+    isElitesAllowed = value;
+}
+
+double Settings::GetEliteRate() const
+{
+    return eliteRate;
+}
+
+void Settings::SetEliteRate(double value)
+{
+    eliteRate = value;
+}
+
+int Settings::GetTournamentSize() const
+{
+    return tournamentSize;
+}
+
+void Settings::SetTournamentSize(int value)
+{
+    tournamentSize = value;
+}
+
+double Settings::GetMutationRate() const
+{
+    return mutationRate;
+}
+
+void Settings::SetMutationRate(double value)
+{
+    mutationRate = value;
+}
+double Settings::GetCrossoverRate() const
+{
+    return crossoverRate;
+}
+
+void Settings::SetCrossoverRate(double value)
+{
+    crossoverRate = value;
+}
--- app/src/mspsp-qt-app/settings.h
+++ app/src/mspsp-qt-app/settings.h
@@ -1,9 +1,43 @@
-#ifndef SETTINGS_H
-#define SETTINGS_H
+#ifndef SetTINGS_H
+#define SetTINGS_H
+
+#include <string>
 
 class Settings
 {
 public:
+
+    // Types of selection methods.
+    enum SELECTION_METHOD
+    {
+        RANK_SELECTION,
+        ROULETTE_WHEEL_SELECTION,
+        TOURNAMENT_SELECTION
+    };
+
+    // Types of crossover methods.
+    enum CROSS_OVER_METHOD
+    {
+        SINGLE_POINT_CROSS_OVER,
+        TWO_POINT_CROSS_OVER,
+        N_POINT_CROSS_OVER,
+        UNIFORM_CROSS_OVER
+    };
+
+    // Types of mutation methods.
+    enum MUTATION_METHOD
+    {
+        SWAP_TEAM_MEMBER_ASSIGNMENT,
+        SWAP_ACTIVITY,
+        SWAP_ACTIVITY_AND_TEAM_MEMBER_ASSIGNMENT
+    };
+
+    // Types of replacement methods.
+    enum REPLACEMENT_METHOD
+    {
+        STEADY_STATE
+    };
+
     Settings();
     Settings(int maximumScheduleLimit, int populationSize);
 
@@ -13,14 +47,57 @@ public:
     int GetPopulationSize() const { return this->populationSize; }
     void SetPopulationSize(int val) { this->populationSize = val; }
 
+    std::string GetFilePath() const;
+    void SetFilePath(const std::string &value);
+
+    double GetCrossoverRate() const;
+    void SetCrossoverRate(double value);
+
+    double GetMutationRate() const;
+    void SetMutationRate(double value);
+
+    int GetTournamentSize() const;
+    void SetTournamentSize(int value);
+
+    double GetEliteRate() const;
+    void SetEliteRate(double value);
+
+    bool GetIsElitesAllowed() const;
+    void SetIsElitesAllowed(bool value);
+
+    int GetEliteSize() const;
+    void SetEliteSize(int value);
+
+    SELECTION_METHOD GetSelectionMethod() const;
+    void SetSelectionMethod(const SELECTION_METHOD &value);
+
+    CROSS_OVER_METHOD GetCrossOverMethod() const;
+    void SetCrossOverMethod(const CROSS_OVER_METHOD &value);
+
+    MUTATION_METHOD GetMutationMethod() const;
+    void SetMutationMethod(const MUTATION_METHOD &value);
+
+    REPLACEMENT_METHOD GetReplacementMethod() const;
+    void SetReplacementMethod(const REPLACEMENT_METHOD &value);
+
 private:
 
     /* Application Settings */
     int maximumScheduleLimit;
+    std::string filePath;
 
     /* Genetic Algorithm Settings */
     int populationSize;
-
+    double crossoverRate;
+    double mutationRate;
+    int tournamentSize;
+    double eliteRate;
+    bool isElitesAllowed;
+    int eliteSize;
+    SELECTION_METHOD selectionMethod;
+    CROSS_OVER_METHOD crossOverMethod;
+    MUTATION_METHOD mutationMethod;
+    REPLACEMENT_METHOD replacementMethod;
 };
 
-#endif // SETTINGS_H
+#endif // SetTINGS_H
